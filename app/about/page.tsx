19bfc75ed58c4e9429c231b7ca26e5ee6b2ab597
oimaster99 <oimaster99@gmail.com>
2024-05-17 12:33:32 -0600

    Modified Navbar

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,8 +3,10 @@ import Link from 'next/link';
 export default function About() {
     return (
       <div>
-        <h1>About Page</h1>
-        <p>This is the about page.</p>
+        <h1>About Us</h1>
+        <p>We are a python education website, designed to teach users grades 6-12.</p>
+        <p>Our mission is to make learning Python easy and accessible to young users.</p>
+
         <div>
           <Link href="/">
             <p>This is an example of an internal link</p>
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,16 +1,22 @@
 import Link from 'next/link';
+import styles from "./navbar.module.css";
 
 export default function NavBar() {
     return (
       <div>
-        <h1>Header</h1>
-        <p>This is the header.</p>
-        <div>
-          <ul>
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/about">About</Link></li>
-          </ul>
-        </div>
+        <main className={styles.main}>
+          <h1>PyCode Educator</h1>
+            <div>
+              <ul>
+                <div className={styles.head}>
+                  <li className={styles.headitem}><Link href="/">Home</Link></li>
+                  <li className={styles.headitem}><Link href="">Learn</Link></li>
+                  <li className={styles.headitem}><Link href="">Lessons</Link></li>
+                  <li className={styles.headitem}><Link href="/about">About</Link></li>
+                </div>
+            </ul>
+          </div>
+        </main>
       </div>
-    );
-}
+      );
+  }
