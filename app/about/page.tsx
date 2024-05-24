e668798334f0045127b034757ea85bcf70e3e46e
Ethan Glenn <ethan+git@eglenn.dev>
2024-05-24 12:10:04 -0600

    Footer, images and some other elements started

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,16 +1,17 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function About() {
+    const name = "Brandon";
+
     return (
-      <div>
-        <h1>About Page</h1>
-        <p>This is the about page.</p>
         <div>
-          <Link href="/">
-            <p>This is an example of an internal link</p>
-          </Link>
+            <h1>About Page Ryan</h1>
+            <p>This is the about {name}</p>
+            <div>
+                <Link href="/">
+                    <p>This is an example of an internal link</p>
+                </Link>
+            </div>
         </div>
-      </div>
-      
     );
-  }
+}
