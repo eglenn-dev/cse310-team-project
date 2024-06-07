c4339a27c8cda1b0ba5ea9396da5ec62686a3cd7
oimaster99 <oimaster99@gmail.com>
2024-06-07 11:30:11 -0600

    Lesson Template Added

---
+++ app/components/textbox.tsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+/*
+class Sharedtextarea extends Component {
+  constructor() {
+    super(props);
+    this.state = {
+      name: "React"
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <label>Enter code : </label>
+        <input type="textare" 
+          name="textValue"
+        />
+      </div>
+    );
+  }
+}
+
+export default Sharedtextarea;
+*/
---
+++ app/lesson/page.tsx
@@ -0,0 +1,36 @@
+import styles from "./page.module.css";
+
+export default function Lesson() {
+  return (
+    <main>
+        <div className={styles.centerHead}>
+            <h1>Lesson 1</h1>
+            <h2>Title of Lesson</h2>
+        </div>
+        <p className={styles.paragraph}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <h3 className={styles.headcode}>Code example</h3>
+        <p className={styles.code}>Python code example goes here!</p>
+        <p className={styles.paragraph}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <h3 className={styles.headcode}>Code example</h3>
+        <p className={styles.code}>More python examples go here!</p>
+
+        <h2>Practice:</h2>
+        <form>
+            <textarea name="pythonCode" rows={4} cols = {40}/>
+          <input type="submit" value="Submit"/>
+        </form>
+    </main>
+  );
+}
