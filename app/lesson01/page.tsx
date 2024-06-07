590c7c83459d6a22a2910105a2d7ecb518238ede
Ethan Glenn <ethan+git@eglenn.dev>
2024-06-07 12:22:57 -0600

    Code color formatting for the lesson pagE

---
+++ app/lesson01/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useEffect } from "react";
+import Prism from "prismjs";
+import "prismjs/themes/prism-tomorrow.css";
+import "prismjs/components/prism-javascript.js";
+import "prismjs/components/prism-python.js";
+import "prismjs/components/prism-jsx.js";
+
+export default function Lesson01() {
+    useEffect(() => {
+        Prism.highlightAll();
+    }, []);
+
+    return (
+        <div>
+            <div>
+                <h1>Welcome to the Python Basics Interactive Tutorial</h1>
+                <p>
+                    Today we will learn about basic syntax, data types, and
+                    basic operations in Python. Let's get started!
+                </p>
+            </div>
+            <div>
+                <p>
+                    In Python, a variable is used to store information.
+                    Variables can store different types of data. Here are some
+                    examples:
+                </p>
+                <pre className="language-python">
+                    <code className="language-python">
+                        {`
+                        # Integer
+                        age = 25
+                        print("Age:", age)
+
+                        # Float
+                        price = 19.99
+                        print("Price:", price)
+
+                        # String
+                        name = "Alice"
+                        print("Name:", name)
+
+                        # Boolean
+                        is_student = True
+                        print("Is student:", is_student)
+                    `}
+                    </code>
+                </pre>
+                <p></p>
+            </div>
+        </div>
+    );
+}
