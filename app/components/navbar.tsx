2d0a97768781a83b3948cea9c06afa81b33d4941
Ethan Glenn <ethan+git@eglenn.dev>
2024-06-12 09:40:05 -0600

    Minor style changes to help with readabilitY

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -5,13 +5,16 @@ import Image from "next/image";
 export default function NavBar() {
     return (
         <div className={styles.navigation}>
-            <Image
-                className={styles.navImage}
-                src="/header-icon.png"
-                width={50}
-                height={50}
-                alt="Header logo icon"
-            />
+            <div className={styles.pageTitle}>
+                <Image
+                    className={styles.navImage}
+                    src="/header-icon.png"
+                    width={50}
+                    height={50}
+                    alt="Header logo icon"
+                />
+                <div>PyCode Educator</div>
+            </div>
             <div className={styles.navLinksList}>
                 <Link className={styles.navLink} href="/">
                     Home
