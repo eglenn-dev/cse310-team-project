fc4fc7557924ae375a8eab985a547653d2169021
Ethan Glenn <ethan+git@eglenn.dev>
2024-06-12 09:24:55 -0600

    New lesson apge data format and pulling from a database

---
+++ app/components/Lesson.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import styles from "./Lesson.module.css";
+import CodeEditor from "./CodeEditor";
+import PythonCodeChunk from "./PythonCodeChunk";
+
+interface LessonDataProps {
+    lessonData: {
+        title: string;
+        description: string;
+        checkpoints: {
+            title: string;
+            description: string;
+            codeExample: string;
+            goal: string;
+            exercise: string;
+        }[];
+    };
+}
+
+const Lesson: React.FC<LessonDataProps> = (lessonData) => {
+    return (
+        <div className={styles.lessonArea}>
+            <div className={styles.header}>
+                <h1>{lessonData.lessonData.title}</h1>
+                <p>{lessonData.lessonData.description}</p>
+            </div>
+            <span className={styles.headerGap}></span>
+            {lessonData.lessonData.checkpoints.map(
+                (checkpoint: any, index: number) => (
+                    <div className={styles.checkpoint}>
+                        <h2>{checkpoint.title}</h2>
+                        <p>{checkpoint.description}</p>
+                        <PythonCodeChunk inputCode={checkpoint.codeExample} />
+                        <div className={styles.exerciseArea}>
+                            <h3>Try your knowledge</h3>
+                            <p>{checkpoint.goal}</p>
+                            <CodeEditor exampleCode={checkpoint.exercise} />
+                        </div>
+                        <div className={styles.submissionButton}>
+                            Check with AI
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                height="1em"
+                                width="1em"
+                            >
+                                <path
+                                    fill="currentColor"
+                                    d="M15 5.25A3.25 3.25 0 0 0 18.25 2h1.5A3.25 3.25 0 0 0 23 5.25v1.5A3.25 3.25 0 0 0 19.75 10h-1.5A3.25 3.25 0 0 0 15 6.75zM4 7a2 2 0 0 1 2-2h7V3H6a4 4 0 0 0-4 4v10a4 4 0 0 0 4 4h12a4 4 0 0 0 4-4v-5h-2v5a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2z"
+                                />
+                            </svg>
+                        </div>
+                    </div>
+                )
+            )}
+        </div>
+    );
+};
+
+export default Lesson;
--- app/page.tsx
+++ app/page.tsx
@@ -5,30 +5,22 @@ import Card from "./components/card";
 export default function Home() {
     return (
         <main className={styles.main}>
+            <div className={styles.welcomeMessage}>
+                <h1>Welcome to PyCode Educator!</h1>
+                <p>
+                    PyCode Educator is a platform for learning Python
+                    programming language. You can learn Python by following the
+                    lessons provided here.
+                </p>
+            </div>
             <div className={styles.lessonArea}>
                 <Card
-                    title="For Loops"
-                    description="Learn the benefits and applications of for loops."
-                    iconPath="/next.svg"
-                    href="/example"
-                    target=""
+                    title="L1: Variables"
+                    description="Learn how to declare and use variables in Python."
+                    iconPath="/container.svg"
+                    href="/lesson01"
                     outbound={false}
-                />
-                <Card
-                    title="If Statements"
-                    description="Learn the basics of If statements and conditionals."
-                    iconPath="/next.svg"
-                    href="/example"
                     target=""
-                    outbound={false}
-                />
-                <Card
-                    title="Inputs and Types"
-                    description="Learn the difference between inputs and types."
-                    iconPath="/next.svg"
-                    href="/example"
-                    target=""
-                    outbound={false}
                 />
             </div>
         </main>
